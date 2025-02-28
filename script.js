23c850f27bfcc84e4759c6fdd2f8e8cbd64c9692
leivas_felipe <163586583+Leivas64@users.noreply.github.com>
2025-02-27 23:58:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const button = document.getElementById('scrollButton');
+const targetSection = document.getElementById('musicas');
+
+  window.addEventListener('scroll', () => {
+        const body = document.body;
+        if (window.scrollY > 100) {
+            body.classList.add('scrolled');
+        } else {
+            body.classList.remove('scrolled');
+        }
+    });
+function rudder() {
+    window.open('https://distrokid.com/hyperfollow/cosmus3/rudder', '_blank');
+}
+function dance() {
+    window.open('https://offstep.link/622550476683')
+}
+function ninehundredninety() {
+    window.open('https://distrokid.com/hyperfollow/cosmus3/990', '_blank');
+}
 function scrollar(sectionNumber) {
     // Gera o ID da seção com base no número passado
     const sectionId = `section${sectionNumber}`;
